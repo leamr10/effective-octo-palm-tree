db7506feef2b2d1095d39b0a9e3cc8e75e247735
leamr10 <leamr10@gmail.com>
2023-06-27 20:40:59 -0400

    for loop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 var apiKey = '7facfa5579c58d1fb4bd2c24b9dea9e0';
-var searchBtn = document.querySelector('search');
+var searchBtn = document.querySelector('#search');
 var searchInput = document.querySelector('input');
 
 searchBtn.addEventListener('click', handleSearch);
@@ -13,7 +13,7 @@ fetchWeather(city)
 }
 
 function fetchWeather(city) {
-    var apiUrlWeather = 'https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial'
+    var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`
     fetch (apiUrlWeather).then(response => response.json ()).then(data => {
         console.log(data);
         displayWeather(data)
@@ -23,20 +23,37 @@ function fetchWeather(city) {
 }
 
 function fetchForecast(data) {
-    var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${apiKey}'
-    fetch(apiUrlForecast).then(response => response.json( )).then(data => (
-        console.log(data)
-    ))
+    var apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${apiKey}`
+    fetch(apiUrlForecast).then(response => response.json( )).then(data => {
+        console.log(data);
+        displayForecast(data.list)
+})
 }
 
 function displayWeather (data) {
+var changeDate = dayjs.unix(data.dt).format('MMM D, YYYY');
 document.getElementById('city').textContent= data.name;
-document.getElementById('date').textContent= data.dt;
-document.getElementById('icon').src='https://openweathermap.org/img/w/${data.weather[0].icon}.png'
+document.getElementById('date').textContent= changeDate;
+document.getElementById('icon').src=`https://openweathermap.org/img/w/${data.weather[0].icon}.png`
 document.getElementById('temp').textContent = data.main.temp + ' F'
 document.getElementById('humid').textContent = data.main.humidity + ' %'
 document.getElementById('wind').textContent = data.wind.speed + ' mph'
 }
 
-var changeDate = dayjs(data.dt).format('MMM, D, YYYY');
-$('#date').text(changeDate);
+function displayForecast (data) {
+    for (let i = 0; i < data.length; i+=8)
+    {
+        let card = document.createElement("div");
+        var changeDate = dayjs.unix(data[i].dt).format('MMM D, YYYY');
+        let date = document.createElement("h3");
+        date.textContent = changeDate;
+        let temp = document.createElement("p");
+        temp.textContent = data[i].main.temp;
+        let humid = document.createElement("p");
+        humid.textContent = data[i].main.humidity;
+        let wind = document.createElement("p");
+        wind.textContent = data[i].wind.speed;
+        let icon = document.createElement("img");
+        icon.src = `https://openweathermap.org/img/w/${data[i].weather[0].icon}.png`
+    }
+}
